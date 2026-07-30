f03db362f28e28efaf2faa85b8125d4f44150f48
rajnikant panchal <rajnikant.panchal95@gmail.com>
2026-07-30 07:17:33 +0530

    fixes issue in preprocessing and training data

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from src import data_preprocessing, train_model, predict, evaluate_model
 models = {}
 
 # data preprocessing
-X_train, y_train, X_test, y_test = data_preprocessing.preprocess()
+X_train, X_test, y_train, y_test = data_preprocessing.preprocess()
 
+# training data
+models = train_model.train(X_train, y_train)
+print(models)
 
 
 
